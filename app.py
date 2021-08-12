53a8a7acc19ee0c0997ea2e491346e19c145726a
thomasajai001 <thomasajaim@outlook.com>
2021-08-12 11:56:50 +0530

    improved ui

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 app = Flask(__name__)
 model = load_model('model/cnn.h5',compile=False)
 
-UPLOAD_FOLDER = 'static/images'
+UPLOAD_FOLDER = 'static'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -23,33 +23,35 @@ def index():
 
 @app.route('/upload',methods=['POST'])
 def upload():
+
     if(request.method=='POST'):
         if  request.files['image']==" ":
-    
             return redirect('/')
         file = request.files['image']
-        # if user does not select file, browser also
-        # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
             return redirect("/")
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # i = image.load_img("static/images/"+filename)
-            image=plt.imread("static/images/"+filename)
-            resized_image=resize(image,(32,32,3))
-            x_t=np.array(resized_image)
-            x_t=x_t.reshape(-1,32,32,3)
+        try:
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                image=plt.imread("static/"+filename)
+                resized_image=resize(image,(32,32,3))
+                x_t=np.array(resized_image)
+                x_t=x_t.reshape(-1,32,32,3)
+                name=model.predict(x_t)
+                index=np.argmax(name)
+                print(dict[index])
+                if(index==9 or index==8):
+                    result={
+                    'image':filename,'result' :dict[index]}  
+                else:
+                    index=8+index%2
+                    result={
+                    'image':filename,'result' :dict[index]}    
+        except (IOError, SyntaxError) as e:
+            result={
+                'error':"Corrupt Image"}  
 
-
-            # i = image.img_to_array(i)/255.0
-            # i = i.reshape(1, 32,32,3)
-            name=model.predict(x_t)
-            index=np.argmax(name)
-            print(dict[index])
-            result={'result' :dict[index],
-            'image':filename}
 
 
         
