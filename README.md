2a53dd901ec30b5da68c1875c2c0d2501cdfbab5
thomasajai001 <thomasajaim@outlook.com>
2021-08-12 13:09:33 +0530

    web page

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # cifar-10-image-classifier
+
+This project was built as part of my submittion to [TinkerHub Foundation](https://tinkerhub.org/) Practical AI Bootcamp Selection Task.
+
+### This is a Image Classifier that can classify images as either Ship or Truck.
+
+
+## Problem Statement
+
+Create a binary classifier to classify images of ships and trucks. Deploy the model and share links to code in GitHub and the API. 
+
+## How does it Work?
+The training dataset for the ML model was obtined from the [CIFAR-10 dataset](https://www.cs.toronto.edu/~kriz/cifar.html) which was loaded from keras inbuilt datasets.
+
+The image classifier is built on Tensorflow framework using CNN architecture.
+
+The model training was done on Google Colab.
+
+[Link to the Jupyter Notebook used](https://colab.research.google.com/drive/1cn-OdyO32lV3pPQWWA5Ddumq4nbew6UU?usp=sharing)
+
+## Webpage
+ [https://cifa-10-classifier.herokuapp.com/]( https://cifa-10-classifier.herokuapp.com/)
+
+ The webpage was built using Flask framework and Bootstrap CSS. It's hosted on heroku.
+
+ ## Built with ❤️ and
+ * [Python3.9](https://docs.python.org/3.7/) - The programming langauge used preprocess data, train the model and build the API.
+* [Tensorflow](https://www.tensorflow.org/) - The core open source library used to develop and train the ML model.
+* [Flask](https://flask.palletsprojects.com/en/2.0.x/) - The micro-web framework
+* [Bootstrap CSS](https://getbootstrap.com/) - The User Interface library used to build the website
--- app.py
+++ app.py
@@ -1,68 +1,63 @@
 import os
-from tensorflow import keras
-import skimage
-from flask import Flask, flash, request, redirect, url_for,render_template
+from flask import Flask, request, redirect, render_template
 from werkzeug.utils import secure_filename
 from keras.models import load_model
-from keras.preprocessing import image
 from skimage.transform import resize
 import numpy as np
 import matplotlib.pyplot as plt
+
 app = Flask(__name__)
-model = load_model('model/cnn.h5',compile=False)
+model = load_model("model/cnn.h5", compile=False)
 
-UPLOAD_FOLDER = 'static'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = "static"
+ALLOWED_EXTENSIONS = set(["txt", "pdf", "png", "jpg", "jpeg", "gif"])
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-dict={0:'airplane',1:'automobile',2:'bird',3:'cat',4:'deer',5:'dog',6:'frog',7:'horse',8:'ship',9:'truck'}
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+dict = {
+    8: "ship",
+    9: "truck",
+}
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload',methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
 
-    if(request.method=='POST'):
-        if  request.files['image']==" ":
-            return redirect('/')
-        file = request.files['image']
-        if file.filename == '':
+    if request.method == "POST":
+        if request.files["image"] == " ":
+            return redirect("/")
+        file = request.files["image"]
+        if file.filename == "":
             return redirect("/")
         try:
             if file:
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                image=plt.imread("static/"+filename)
-                resized_image=resize(image,(32,32,3))
-                x_t=np.array(resized_image)
-                x_t=x_t.reshape(-1,32,32,3)
-                name=model.predict(x_t)
-                index=np.argmax(name)
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+                image = plt.imread("static/" + filename)
+                resized_image = resize(image, (32, 32, 3))
+                x_t = np.array(resized_image)
+                x_t = x_t.reshape(-1, 32, 32, 3)
+                name = model.predict(x_t)
+                index = np.argmax(name)
                 print(dict[index])
-                if(index==9 or index==8):
-                    result={
-                    'image':filename,'result' :dict[index]}  
+                if index >7:
+                    result = {"image": filename, "result": dict[index]}
                 else:
-                    index=8+index%2
-                    result={
-                    'image':filename,'result' :dict[index]}    
+                    index = 8 + index % 2
+                    result = {"image": filename, "result": dict[index]}
         except (IOError, SyntaxError) as e:
-            result={
-                'error':"Corrupt Image"}  
+            result = {"error": "Corrupt Image"}
 
+    return render_template("result.html", result=result)
 
-
-        
-    return render_template("result.html",result = result)
 
 @app.route("/result")
 def result():
-    return render_template('result.html')
-
-
-
+    return render_template("result.html")
 
 
 if __name__ == "__main__":
